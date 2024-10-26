47b76a58efdd0ffe2dcc4d03a207da5b279a4b3a
Mikhail Orlov <crazyminer398@gmail.com>
2024-10-26 19:48:28 +0200

    feat(Calculator): finished calculator

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::io;
+use std::io::Write;
+
+use console::Term;
+
 fn main() {
-    println!("Hello, world!");
+    let mut first_number: String = String::new();
+    let mut second_number: String = String::new();
+    let mut operation: Term = Term::stdout();
+    let result: i128;
+
+    print!("Enter the first number: ");
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut first_number)
+        .expect("Failed to read line");
+
+    print!("Enter the second number: ");
+    io::stdout().flush().unwrap();
+
+    io::stdin()
+        .read_line(&mut second_number)
+        .expect("Failed to read line");
+
+    let first_number: i128 = first_number.trim().parse().expect("Please type a number");
+    let second_number: i128 = second_number.trim().parse().expect("Please type a number");
+
+    print!("Select the operation (+, -, *, /): ");
+    io::stdout().flush().unwrap();
+
+    let operation: char = Term::read_char(&mut operation)
+        .expect("Please type a valid character");
+
+    println!("{operation}");
+
+    match operation {
+        '+' => result = first_number + second_number,
+        '-' => result = first_number - second_number,
+        '*' => result = first_number * second_number,
+        '/' => result = first_number / second_number,
+        _ => panic!("Please type a valid operation")
+    }
+
+    println!("Result: {}", result);
 }
